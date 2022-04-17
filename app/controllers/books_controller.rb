8fa3f2c6e656fe08ba200eb1a3a86b241773cee0
eddie <eddismmm@gmail.com>
2022-04-17 22:38:23 +0800

    update all

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,52 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def new
-    @books = Book.new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "新增書目成功！"
+      redirect_to "/books"
+    else
+      render :new
+    end
   end
 
   def show
+    @book = Book.find_by(id: params[:id])
   end
+
+  def edit
+    @book = Book.find_by(id: params[:id])
+  end
+
+  def update
+    @book = Book.find_by(id: params[:id])
+
+    if @book.update(book_params)
+        flash[:notice] = "書目已更新!"
+        redirect_to '/books'
+    else
+        render :edit
+    end
+end
+
+def destroy
+  @book = Book.find_by(id: params[:id])
+  @book.destroy
+
+  flash[:notice] = "書目已刪除!"
+  redirect_to '/books'
+end
+
+  private
+  def book_params
+    params.require(:book).permit(:name, :date, :price, :page, :ISBN)
+  end
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :date, presence: true
+    validates :price, numericality: {greater_than: 0}
+    validates :page, numericality: {greater_than: 0}
+    validates :ISBN, presence: true, uniqueness: true
 end
